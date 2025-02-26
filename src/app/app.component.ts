9363ae141e262b4812967c1f41c03077b8a9df58
mariemabdelsalam <mariemabdelsalam61@gmail.com>
2025-02-26 11:45:41 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { MeallayoutComponent } from "./components/meallayout/meallayout.componen
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MeallayoutComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,12 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { MealsComponent } from './components/meals/meals.component';
+import { MealdetailsComponent } from './components/mealdetails/mealdetails.component';
 
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
     renderMode: RenderMode.Prerender
-  }
+  },
+  { path: "category/:name", renderMode: RenderMode.Client },
+  { path: 'details/:id', renderMode: RenderMode.Client }
 ];
